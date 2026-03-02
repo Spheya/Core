71087e41fc00c7f73fd57e82f253217ce220b478
Spheya <tamara@spheya.com>
2026-03-02 16:58:49 +0100

    move game and render logic to virtual screenspace

--- src/rendering/surface.hpp
+++ src/rendering/surface.hpp
@@ -50,6 +50,19 @@ public:
 
 	[[nodiscard]] IDCompositionTarget* getTarget() const { return m_target.Get(); }
 	[[nodiscard]] IDCompositionVisual* getVisual() const { return m_visual.Get(); }
+	[[nodiscard]] glm::mat4 getProjectionMatrix() const {
+		glm::vec2 p = getPosition();
+		glm::vec2 s = getDimensions();
+
+		// clang-format off
+		return glm::mat4(
+			+2.0f / s.x, 0.0f, 0.0f, -1.0f - 2.0 * p.x / s.x,
+			0.0f, -2.0f / s.y, 0.0f, +1.0f + 2.0 * p.y / s.y,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f
+		);
+		// clang-format on
+	}
 
 private:
 	ComPtr<IDCompositionTarget> m_target;
